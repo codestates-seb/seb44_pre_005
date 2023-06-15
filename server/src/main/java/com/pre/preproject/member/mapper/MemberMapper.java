fee78d3c9fd23cf8d7a584b989e1afb932bc8624
pparkjyy <jyp96117@naver.com>
2023-06-15 14:15:09 +0900

    feat/mapper,Service(v1)

--- server/src/main/java/com/pre/preproject/member/mapper/MemberMapper.java
+++ server/src/main/java/com/pre/preproject/member/mapper/MemberMapper.java
@@ -1,4 +1,12 @@
 package com.pre.preproject.member.mapper;
 
-public class MemberMapper {
+import com.pre.preproject.member.dto.MemberDto;
+import com.pre.preproject.member.entity.Member;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+    Member PostDtoToEntity(MemberDto.Post postDto);
+
+    MemberDto.Response EntityToResponseDto(Member member);
 }
--- server/src/main/java/com/pre/preproject/member/service/MemberService.java
+++ server/src/main/java/com/pre/preproject/member/service/MemberService.java
@@ -1,4 +1,18 @@
 package com.pre.preproject.member.service;
 
+import com.pre.preproject.member.entity.Member;
+import com.pre.preproject.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+    // private final CustomAuthorityUtils authorityUtils;
+    private final PasswordEncoder passwordEncoder;
+
 }
