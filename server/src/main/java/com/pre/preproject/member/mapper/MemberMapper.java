82019834e4363a89058207361624237ad614ad1a
Seohyun-Back <lucymail100@naver.com>
2023-06-15 14:35:00 +0900

    MemberService, MultiResponDto, SingleResonseDto 없는 상태로 Controller 추가 Dto 함수명, 파라미터명 수정 및 추가 Utils_UriCreator 추가

--- server/src/main/java/com/pre/preproject/member/mapper/MemberMapper.java
+++ server/src/main/java/com/pre/preproject/member/mapper/MemberMapper.java
@@ -1,12 +1,16 @@
 package com.pre.preproject.member.mapper;
 
+
 import com.pre.preproject.member.dto.MemberDto;
 import com.pre.preproject.member.entity.Member;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
     Member PostDtoToEntity(MemberDto.Post postDto);
-
-    MemberDto.Response EntityToResponseDto(Member member);
+    Member PatchDtoToEntity(MemberDto.Patch patchDto);
+    MemberDto.Response memberToMemberResponse(Member member);
+    List<MemberDto.Response> membersToMemberResponses(List<Member> members);
 }
