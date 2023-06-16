e2a33f07242e55dcae844a562940412b91fe1000
황찬우 <49116370+HChanWoo@users.noreply.github.com>
2023-06-16 10:22:12 +0900

    Delete client/src directory

--- client/src/components/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import tw from "tailwind-styled-components";
-
-export default function Footer() {
-  return <div></div>;
-}
--- client/src/components/Header.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import tw from "tailwind-styled-components";
-
-export default function Header() {
-  return <div></div>;
-}
