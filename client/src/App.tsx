a48595d0198774b97f142b37aa51892aa6adb5f2
HChanWoo <cksdn1st@naver.com>
2023-06-16 09:56:28 +0900

    fix: fix setting to tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Detail from "./page/Detail";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Detail />}></Route>
-      </Routes>
-      <Footer />
+      <Routes>{/* <Route path="/" element={<Detail />}></Route> */}</Routes>
     </div>
   );
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import tw from "tailwind-styled-components";
 import { Link } from "react-router-dom";
 
-import logo from "./stackoverflow.PNG";
-
 export default function Header() {
   return (
     <nav>
       <div className="navbar">
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
+        <Link to="/">hi</Link>
       </div>
     </nav>
   );
