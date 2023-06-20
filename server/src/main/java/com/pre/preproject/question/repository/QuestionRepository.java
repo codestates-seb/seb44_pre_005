c38ae61cbd277c4551a86310b9119992b6fd1e2f
devhanda <124903073+devhanda@users.noreply.github.com>
2023-06-20 11:46:46 +0900

    Revert "Feat/question CRUD"

--- server/src/main/java/com/pre/preproject/question/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pre.preproject.question.repository;
-
-import com.pre.preproject.question.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question,Long> {
-}
